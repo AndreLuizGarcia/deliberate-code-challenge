797efc4ccf7474801be37830f078098acc5dcdf9
AndreLuizGarcia <andre.luiz@gec.inatel.br>
2022-05-02 22:54:30 -0300

    feat: add config files

--- .jest/setup.ts
+++ .jest/setup.ts
@@ -1,1 +1,7 @@
+import React from 'react'
 import '@testing-library/jest-dom'
+
+import { configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+configure({ adapter: new Adapter() });
--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,19 @@ module.exports = {
   testEnvironment: 'jsdom',
   testPathIgnorePatterns: ['/node_modules'],
   collectCoverage: true,
-  collectCoverageFrom: ['src/**/*.ts(x)?', '!src/**/stories.tsx'],
-  setupFilesAfterEnv: ['<rootDir>/.jest/setup.ts']
+  collectCoverageFrom: [
+    'src/**/*.ts(x)?',
+    '!src/**/stories.tsx',
+    '!src/**/styles.ts',
+    '!src/types/*',
+    '!src/styles/*',
+    '!src/services/*',
+    '!src/index.tsx'
+  ],
+  setupFilesAfterEnv: ['<rootDir>/.jest/setup.ts'],
+  preset: 'ts-jest',
+  transform: {
+    '^.+\\.(ts|tsx)?$': 'ts-jest',
+    '^.+\\.(js|jsx)$': 'babel-jest'
+  }
 }
