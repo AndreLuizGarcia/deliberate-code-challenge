a92d0c874f5a1f1a99ea08402ade8b9e7a8bd6bc
AndreLuizGarcia <andre.luiz@gec.inatel.br>
2022-05-02 23:06:02 -0300

    feat: update globalStyles

--- src/styles/globalStyles.ts
+++ src/styles/globalStyles.ts
@@ -1,4 +1,4 @@
-import { createGlobalStyle } from 'styled-components'
+import styled, { createGlobalStyle } from 'styled-components'
 
 const GlobalStyles = createGlobalStyle`
   * {
@@ -8,12 +8,33 @@ const GlobalStyles = createGlobalStyle`
   }
 
   html {
-    font-size: 62.5%;
+    font-size: 16px;
   }
 
   body {
     font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif
+    height: 100vh;
   }
+`
+
+export const Wrapper = styled.div`
+  background-color: #5479f7;
+
+  display: flex;
+  justify-content: center;
+`
+
+export const Grid = styled.div`
+  margin: 0 32px;
+
+  width: 1000px;
+  height: 100%;
+  min-height: 100vh;
+
+  display: flex;
+  flex-direction: column;
+
+  margin-bottom: 32px;
 `
 
 export default GlobalStyles
